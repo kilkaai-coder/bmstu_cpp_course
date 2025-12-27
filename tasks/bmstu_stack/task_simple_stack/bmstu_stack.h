157de32b3114803cd3486562430712bec6dbb832
kilkaai-coder <kilkaai07@gmail.com>
2025-12-27 15:06:59 +0300

    refactor: update stack to working version

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -1,132 +1,138 @@
 #pragma once
 
-#include <exception>
-#include <utility>
-#include <cstddef>
 #include <new>
+#include <stdexcept>
+#include <utility>
 
 namespace bmstu
 {
 template <typename T>
 class stack
 {
-private:
-    T* data_ = nullptr;
-    size_t size_ = 0;
-    size_t capacity_ = 0;
-
-    void reserve(size_t new_capacity) {
-        if (new_capacity <= capacity_) return;
-        
-        T* new_data = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
-        
-        for (size_t i = 0; i < size_; ++i) {
-            new (&new_data[i]) T(std::move(data_[i]));
-            data_[i].~T();
-        }
-        
-        ::operator delete(data_);
-        data_ = new_data;
-        capacity_ = new_capacity;
-    }
-
 public:
-    stack() = default;
-    
-    ~stack() {
-        for (size_t i = 0; i < size_; ++i) data_[i].~T();
-        ::operator delete(data_);
-    }
-    
-    stack(const stack& other) : data_(nullptr), size_(0), capacity_(0) {
-        if (other.size_ > 0) {
-            capacity_ = other.size_;
-            data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
-            for (size_t i = 0; i < other.size_; ++i) {
-                new (&data_[size_++]) T(other.data_[i]);
-            }
-        }
-    }
-    
-    stack& operator=(const stack& other) {
-        if (this != &other) {
-            for (size_t i = 0; i < size_; ++i) data_[i].~T();
-            size_ = 0;
-            
-            if (capacity_ < other.size_) {
-                ::operator delete(data_);
-                capacity_ = other.size_;
-                data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
-            }
-            
-            for (size_t i = 0; i < other.size_; ++i) {
-                new (&data_[size_++]) T(other.data_[i]);
-            }
-        }
-        return *this;
-    }
-    
-    stack(stack&& other) noexcept 
-        : data_(other.data_), size_(other.size_), capacity_(other.capacity_) {
-        other.data_ = nullptr;
-        other.size_ = 0;
-        other.capacity_ = 0;
-    }
-    
-    stack& operator=(stack&& other) noexcept {
-        if (this != &other) {
-            for (size_t i = 0; i < size_; ++i) data_[i].~T();
-            ::operator delete(data_);
-            
-            data_ = other.data_;
-            size_ = other.size_;
-            capacity_ = other.capacity_;
-            
-            other.data_ = nullptr;
-            other.size_ = 0;
-            other.capacity_ = 0;
-        }
-        return *this;
-    }
-    
-    bool empty() const { return size_ == 0; }
-    size_t size() const { return size_; }
-    
-    void clear() {
-        for (size_t i = 0; i < size_; ++i) data_[i].~T();
-        size_ = 0;
-    }
-    
-    void push(const T& value) {
-        if (size_ == capacity_) reserve(capacity_ + 1);  // ЛИНЕЙНЫЙ РОСТ!
-        new (&data_[size_++]) T(value);
-    }
-    
-    void push(T&& value) {
-        if (size_ == capacity_) reserve(capacity_ + 1);  // ЛИНЕЙНЫЙ РОСТ!
-        new (&data_[size_++]) T(std::move(value));
-    }
-    
-    template <typename... Args>
-    void emplace(Args&&... args) {
-        if (size_ == capacity_) reserve(capacity_ + 1);  // ЛИНЕЙНЫЙ РОСТ!
-        new (&data_[size_++]) T(std::forward<Args>(args)...);
-    }
-    
-    void pop() {
-        if (size_ == 0) throw std::underflow_error("Stack is empty");
-        data_[--size_].~T();
-    }
-    
-    T& top() {
-        if (size_ == 0) throw std::underflow_error("Stack is empty");
-        return data_[size_ - 1];
-    }
-    
-    const T& top() const {
-        if (size_ == 0) throw std::underflow_error("Stack is empty");
-        return data_[size_ - 1];
-    }
+	stack() noexcept : data_(nullptr), size_(0u) {}
+
+	~stack() { clear(); }
+
+	bool empty() const noexcept { return size_ == 0u; }
+
+	size_t size() const noexcept { return size_; }
+
+	template <typename... Args>
+	void emplace(Args&&... args)
+	{
+		const size_t new_size = size_ + 1u;
+		T* new_data = allocate_raw(new_size);
+
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (new_data + i) T(std::move(data_[i]));
+		}
+
+		new (new_data + (new_size - 1u)) T(std::forward<Args>(args)...);
+
+		destroy_all();
+		deallocate_raw(data_);
+
+		data_ = new_data;
+		size_ = new_size;
+	}
+
+	void push(const T& value)
+	{
+		const size_t new_size = size_ + 1u;
+		T* new_data = allocate_raw(new_size);
+
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (new_data + i) T(std::move(data_[i]));
+		}
+
+		new (new_data + (new_size - 1u)) T(value);
+
+		destroy_all();
+		deallocate_raw(data_);
+
+		data_ = new_data;
+		size_ = new_size;
+	}
+
+	void push(T&& value)
+	{
+		const size_t new_size = size_ + 1u;
+		T* new_data = allocate_raw(new_size);
+
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (new_data + i) T(std::move(data_[i]));
+		}
+
+		new (new_data + (new_size - 1u)) T(std::move(value));
+
+		destroy_all();
+		deallocate_raw(data_);
+
+		data_ = new_data;
+		size_ = new_size;
+	}
+
+	void pop()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("stack is empty");
+		}
+
+		data_[size_ - 1u].~T();
+		--size_;
+	}
+
+	void clear() noexcept
+	{
+		destroy_all();
+		deallocate_raw(data_);
+		data_ = nullptr;
+		size_ = 0u;
+	}
+
+	T& top()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("stack is empty");
+		}
+		return data_[size_ - 1u];
+	}
+
+	const T& top() const
+	{
+		if (empty())
+		{
+			throw std::underflow_error("stack is empty");
+		}
+		return data_[size_ - 1u];
+	}
+
+private:
+	static T* allocate_raw(size_t n)
+	{
+		return static_cast<T*>(::operator new(sizeof(T) * n));
+	}
+
+	static void deallocate_raw(T* ptr) noexcept
+	{
+		::operator delete(ptr);
+	}
+
+	void destroy_all() noexcept
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
+			data_[i].~T();
+		}
+	}
+
+	T* data_;
+	size_t size_;
 };
-}
-//trigger
+}  // namespace bmstu
